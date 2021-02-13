dca8bbc58ed3d46004010e4246dfc686b07c683c
p23bgalicia <77790781+p23bgalicia@users.noreply.github.com>
2021-02-13 12:50:54 -0500

    Create hw3-2-3.js
    This is hw3-2 problem #3

---
+++ hw3-2-3.js
@@ -0,0 +1,11 @@
+// Author: BDG (AMDG) 2/13/2021
+
+var myPassword = 1234;
+var strongPassword = 3521478490;
+
+if (myPassword > 3521478490){
+    console.log("You have a stong passord");
+} 
+else{
+    console.log("You have a weak password");
+}
