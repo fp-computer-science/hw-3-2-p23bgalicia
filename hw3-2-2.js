b2a8380c62af63154cb4fa389484040ca0fa21a8
p23bgalicia <77790781+p23bgalicia@users.noreply.github.com>
2021-02-13 12:25:56 -0500

    Create hw3-2-2.js
    This is hw3-2 problem #2

---
+++ hw3-2-2.js
@@ -0,0 +1,10 @@
+// Author: BDG (AMDG) 2/12/2021
+
+var kinghtAttack = 90;
+var dragonDefense = 85;
+
+if (kinghtAttack > 85){
+    console.log("Sucess, you slayed the dragon");
+} else {
+    console.log("You were not succesful at slaying the dragon");
+}
