b0e537822e16f899330035c0ec045047e23e2092
p23bgalicia <77790781+p23bgalicia@users.noreply.github.com>
2021-02-13 12:24:31 -0500

    Create hw3-2-1.js
    This is homework 3-2 problem#2

---
+++ hw3-2-1.js
@@ -0,0 +1,9 @@
+// Author: BDG (AMDG) 2/12/2021
+
+var playerSpeed = 95;
+
+if (playerSpeed >= 80){
+    console.log("Your were succesful at making the jump");
+} else {
+    console.log("You can't make the jump");
+}
